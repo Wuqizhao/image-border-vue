046cf1e7afb5f90e7d7c2bf14a2adea6d1428e79
sunshj <47186980+sunshj@users.noreply.github.com>
2025-04-16 19:59:17 +0800

    build: 添加 vite-plugin-vue-devtools 以支持 Vue 开发工具

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
 	server: {
 		host: "0.0.0.0", // 允许外部访问
 	},
-	plugins: [vue()],
+	plugins: [vue(), vueDevTools()],
 	// 配置别名
 	resolve: {
 		alias: {
